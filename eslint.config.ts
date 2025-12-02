17f82b8db32581d6ee5874704ae5c4d5d005a871
Illich Rada <49048159+illich570@users.noreply.github.com>
2025-12-02 17:20:21 -0300

    Feature/new workflow (#1)
    * [ADD] modification to GH worfklow
    
    * [REF] modification to corepack
    
    * [REF] new modification to corepack
    
    * [REF modified worfklow
    
    * [FIX] change pnpm version in workflow
    
    * [ADD] added husky and lint-staged
    
    * [REF] modified lint-staged commands
    
    * [REF] better workflow and removed dist folder
    
    * [ADD] initial testing config
    
    * [FIX] fixed argument in workflow ci

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,51 +1,53 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import pluginJest from "eslint-plugin-jest";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import pluginJest from 'eslint-plugin-jest';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    ignores: ['**/*.js', 'dist/**/*', 'node_modules/**/*'],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  eslintPluginUnicorn.configs["recommended"],
+  eslintPluginUnicorn.configs['recommended'],
   {
-    files: ["**/*.{js,ts}"],
-    ignores: ["**/*.js", "dist/**/*", "node_modules/**/*"],
+    files: ['**/*.{js,ts}'],
+    ignores: ['**/*.js', 'dist/**/*', 'node_modules/**/*'],
     plugins: {
-      "simple-import-sort": simpleImportSort,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "unicorn/better-regex": "warn",
-      "unicorn/no-process-exit": "off",
-      "unicorn/no-array-reduce": "off",
-      "unicorn/prevent-abbreviations": [
-        "error",
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unicorn/better-regex': 'warn',
+      'unicorn/no-process-exit': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
         { replacements: { params: false } },
       ],
     },
   },
   {
-    files: ["src/**/*.test.{js,ts}"],
+    files: ['src/**/*.test.{js,ts}'],
     plugins: { jest: pluginJest },
+    ignores: ['**/*.js', 'dist/**/*', 'node_modules/**/*'],
     languageOptions: {
       globals: pluginJest.environments.globals.globals,
     },
     rules: {
-      "jest/no-disabled-tests": "warn",
-      "jest/no-focused-tests": "error",
-      "jest/no-identical-title": "error",
-      "jest/prefer-to-have-length": "warn",
-      "jest/valid-expect": "error",
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
     },
   },
   eslintPluginPrettierRecommended,
