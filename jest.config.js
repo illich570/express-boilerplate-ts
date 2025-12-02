b22fd871751acecf6a704d9738d8003f0892f880
Illich Rada <illich570@gmail.com>
2025-12-02 16:02:37 -0400

    [ADD] initial testing config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
-import { createDefaultPreset } from 'ts-jest';
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  transform: {
-    ...tsJestTransformCfg,
+  testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  verbose: true,
 };
